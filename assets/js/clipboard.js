c3e810822ec3539521a68ff1523619a74c4072b5
ghostsmpofficial <ghost.smp.offficial@gmail.com>
2024-12-10 10:34:41 +0700

    Create clipboard.js

---
+++ assets/js/clipboard.js
@@ -0,0 +1,10 @@
+function copy(){
+    navigator.clipboard.writeText('node.cyberdigitalshop.akashona.cloud:19138')
+    swal({
+    title: 'IP BERHASIL DI COPPY',
+    text: 'SELAMAT BERSENANG-SENANG',
+    icon: 'success',
+    timer: 2000,
+    buttons: false,
+})
+}
