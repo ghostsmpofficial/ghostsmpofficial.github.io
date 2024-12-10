2839a28e771ccc1428e9f11253f82da628099268
ghostsmpofficial <ghost.smp.offficial@gmail.com>
2024-12-10 10:33:27 +0700

    Create clipboard_teamspeak.js

---
+++ assets/js/clipboard_teamspeak.js
@@ -0,0 +1,10 @@
+function copy(){
+    navigator.clipboard.writeText('ts.onecraft.it')
+    swal({
+    title: 'IP BERHASIL DI COPPY',
+    text: 'SELAMAT BERSENANG-SENANG',
+    icon: 'success',
+    timer: 2000,
+    buttons: false,
+})
+}
